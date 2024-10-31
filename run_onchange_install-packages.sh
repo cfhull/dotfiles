094341ed69d270502eae04a94c188a6e6bb3ddfb
Chris Hull <chris.f.hull@gmail.com>
2024-10-31 10:50:20 -0400

    install brew in non-interactive mode

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install nvim ripgrep
 
