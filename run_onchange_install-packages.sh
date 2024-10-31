b6f9ae0a7033d9af63cca40e9260a74ed1abaf9c
Chris Hull <chris.f.hull@gmail.com>
2024-10-31 10:53:24 -0400

    add brew to path

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Install brew and add to path
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /home/node/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/node/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Install dev dependencies
 brew install nvim ripgrep
 
